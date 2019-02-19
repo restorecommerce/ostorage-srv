5541ae543249ad57f654c6b065f4fa66c12bd2c0
Andrei Larin <andrei.larin@n-fuse.co>
2019-02-19 15:33:57 +0200

    Added delay for tests not to fail

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,6 @@ import * as mocha from 'mocha';
 import * as should from 'should';
 import * as assert from 'assert';
 import { Worker } from '../worker';
-import * as Logger from '@restorecommerce/logger';
 import * as grpcClient from '@restorecommerce/grpc-client';
 import * as kafkaClient from '@restorecommerce/kafka-client';
 import * as sconfig from '@restorecommerce/service-config';
@@ -16,84 +15,97 @@ let client;
 let worker: Worker;
 // For event listeners
 let events;
-let topic;
+let oStorage;
+let itemKey;
 
 async function start(): Promise<void> {
-    cfg = sconfig(process.cwd() + '/test');
-    worker = new Worker(cfg);
-    await worker.start();
+  cfg = sconfig(process.cwd() + '/test');
+  worker = new Worker(cfg);
+  await worker.start();
 }
 
 async function stop(): Promise<void> {
-    await worker.stop();
+  await worker.stop();
+}
+
+// wait function since there's no such thing in TS
+// TODO use some framework instead
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
 }
 
 async function connect(clientCfg: string, resourceName: string): Promise<any> { // returns a gRPC service
-    logger = worker.logger;
+  logger = worker.logger;
 
-    events = new Events(cfg.get('events:kafka'), logger);
-    await (events.start());
+  events = new Events(cfg.get('events:kafka'), logger);
+  await (events.start());
 
-    client = new grpcClient.Client(cfg.get(clientCfg), logger);
-    const service = await client.connect();
-    return service;
+  client = new grpcClient.Client(cfg.get(clientCfg), logger);
+  const service = await client.connect();
+  return service;
 }
 
 describe('testing ostorage-srv', () => {
-    before(async function startServer(): Promise<void> {
-        await start();
+  before(async function startServer(): Promise<void> {
+    await start();
+  });
+
+
+  after(async function stopServer(): Promise<void> {
+    await stop();
+  });
+
+  describe('Testing ostorage methods', () => {
+
+    it('Should be empty', async () => {
+      oStorage = await connect('grpc-client:service-ostorage', '');
+      let result = await oStorage.list();
+      should(result.data.file_information).empty;
     });
 
+    it('Should add data to ostorage', async () => {
+      let meta = {
+        modified_by: 'SYSTEM',
+        owner: [{
+          id: 'urn:restorecommerce:acs:names:ownerIndicatoryEntity',
+          value: 'urn:restorecommerce:acs:model:user.User'
+        },
+        {
+          id: 'urn:restorecommerce:acs:names:ownerInstance',
+          value: 'UserID'
+        }]
+      };
 
-    after(async function stopServer(): Promise<void> {
-        await stop();
+      let result = await oStorage.put({
+        bucket: 'invoices',
+        key: 'test_object_123',
+        object: "Test object1",
+        meta
+      });
+      itemKey = result.data.key;
+      should(result.error).null;
+      should.exist(result.data.bucket);
+      should.exist(result.data.key);
+      wait(15);
     });
-
-    describe('Testing ostorage methods', () => {
-
-        it('Should be empty', async () => {
-            let oStorage = await connect('grpc-client:service-ostorage', '');
-            let result = await oStorage.list();
-            should(result.data.file_information).empty;
-        });
-
-        it('Should add data to ostorage', async () => {
-            let oStorage = await connect('grpc-client:service-ostorage', '');
-            let meta = {
-                modified_by: 'SYSTEM',
-                owner: [{
-                  id: 'urn:restorecommerce:acs:names:ownerIndicatoryEntity',
-                  value: 'urn:restorecommerce:acs:model:user.User'
-                },
-                {
-                  id: 'urn:restorecommerce:acs:names:ownerInstance',
-                  value: 'UserID'
-                }]
-            };
-
-            let result = await oStorage.put({
-                bucket: 'invoices',
-                key: 'test_object_234',
-                object: "Test object.",
-                meta: meta
-            });
-            should(result.error).null;
-            should.exist(result.data.bucket);
-            should.exist(result.data.key);
-            
-            var itemKey = result.data.key;
-            var itemBucket = result.data.bucket;
-            result = await oStorage.list({
-                bucket: itemBucket
-            });
-            should(result.data.file_information).length(1);
-
-            result = await oStorage.delete({
-                bucket: itemBucket,
-                key: itemKey
-            });
-            should(result.error).null;
-            should(result.data).empty;
-        });
+    it('should list the added files', async () => {
+      let result = await oStorage.list({
+        bucket: 'invoices'
+      });
+      should(result.data.file_information).length(1);
+      wait(15);
     });
+    it('should delete the object', async () => {
+      let result = await oStorage.delete({
+        bucket: 'invoices',
+        key: itemKey
+      });
+      should(result.error).null;
+      should(result.data).empty;
+    });
+  });
 });
