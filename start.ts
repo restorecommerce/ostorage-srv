3c6b35486f542936dfc21044368c73d854739b2f
akumar <arun.kumar@n-fuse.co>
2019-07-11 15:49:30 +0200

    up deps

--- start.ts
+++ start.ts
@@ -3,3 +3,6 @@ import * as Cluster from '@restorecommerce/cluster-service';
 const cfg = require('@restorecommerce/service-config')(process.cwd());
 const server = new Cluster(cfg);
 server.run('./worker');
+process.on('SIGINT', () => {
+    server.stop();
+});
